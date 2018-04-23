6168ca47d43354e4e551ecbd0e08dea2b3b343f2
Deepak Singh Negi <deepaksingh@ogrelogic.com>
2018-04-23 17:11:25 +0530

    Map feature added

---
+++ GTExample/MapForItinary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinary.swift
+//  GTExample
+//
+//  Created by Deepak Singh Negi on 4/23/18.
+//  Copyright © 2018 OgreLogic LLC. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinary: NSObject {
+
+}
